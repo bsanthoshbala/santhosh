1b3b15b5c94af7336962991850b23d391cc429d7
bsanthoshbala <bsanthoshbala@gmail.com>
2022-07-10 12:31:41 +0530

    san

---
+++ src/com/tset/san.java
@@ -0,0 +1,13 @@
+package com.tset;
+
+public class san {
+	private void test1() {
+		System.out.println("test1");
+
+}
+	public static void main(String[] args) {
+		san s = new san();
+		s.test1();
+	}
+		
+	}
